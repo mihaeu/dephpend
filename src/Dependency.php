c57522bb00eef0c6b2de29d9859a59609b45c906
Michael Haeuslmann <haeuslmann@gmail.com>
2016-06-07 11:34:33 +0200

    Add value object for dependencies

---
+++ src/Dependency.php
@@ -0,0 +1,40 @@
+<?php
+
+declare (strict_types = 1);
+
+namespace Mihaeu\PhpDependencies;
+
+class Dependency
+{
+    /** @var Clazz */
+    private $from;
+
+    /** @var Clazz */
+    private $to;
+
+    /**
+     * @param Clazz $from
+     * @param Clazz $to
+     */
+    public function __construct(Clazz $from, Clazz $to)
+    {
+        $this->from = $from;
+        $this->to = $to;
+    }
+
+    /**
+     * @return Clazz
+     */
+    public function from() : Clazz
+    {
+        return $this->from;
+    }
+
+    /**
+     * @return Clazz
+     */
+    public function to() : Clazz
+    {
+        return $this->to;
+    }
+}
---
+++ tests/DependencyTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare (strict_types = 1);
+
+namespace Mihaeu\PhpDependencies;
+
+/**
+ * @covers Mihaeu\PhpDependencies\Dependency
+ *
+ * @covers Mihaeu\PhpDependencies\Clazz
+ */
+class DependencyTest extends \PHPUnit_Framework_TestCase
+{
+    public function testHasClazzFrom()
+    {
+        $this->assertEquals(new Clazz('From'), (new Dependency(new Clazz('From'), new Clazz('To')))->from());
+    }
+
+    public function testHasClazzTo()
+    {
+        $this->assertEquals(new Clazz('To'), (new Dependency(new Clazz('From'), new Clazz('To')))->to());
+    }
+}
