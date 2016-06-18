c1f8b2fc8955e3780f02c94b3efa7dcc6dfd7c71
Michael Haeuslmann <haeuslmann@gmail.com>
2016-06-18 09:27:33 +0200

    Add test for detection of extends and implements

--- tests/DependencyInspectionVisitorTest.php
+++ tests/DependencyInspectionVisitorTest.php
@@ -8,6 +8,7 @@ use PhpParser\Node\Expr\New_ as NewNode;
 use PhpParser\Node\Expr\Variable as VariableNode;
 use PhpParser\Node\Name\FullyQualified as FullyQualifiedNameNode;
 use PhpParser\Node\Stmt\Class_ as ClassNode;
+use PhpParser\Node\Stmt\Interface_ as InterfaceNode;
 
 /**
  * @covers Mihaeu\PhpDependencies\DependencyInspectionVisitor
@@ -78,5 +79,47 @@ class DependencyInspectionVisitorTest extends \PHPUnit_Framework_TestCase
             ->dependencies()
             ->findClassesDependingOn(new Clazz('SomeNamespace.SomeClass'));
         $this->assertEquals(new Clazz('$testDep'), $classesDependingOnSomeClass->toArray()[0]);
+    }
+
+    public function testDetectsExtendedClasses()
+    {
+        $node = new ClassNode('SomeClass');
+        $node->namespacedName = new \stdClass();
+        $node->namespacedName->parts = ['SomeNamespace', 'SomeClass'];
+        $node->extends = new \stdClass();
+        $node->extends->parts = ['SomeSuperClass'];
+        $this->dependencyInspectionVisitor->enterNode($node);
+
+        $this->dependencyInspectionVisitor->afterTraverse([]);
+        $classesDependingOnSomeClass = $this->dependencyInspectionVisitor
+            ->dependencies()
+            ->findClassesDependingOn(new Clazz('SomeNamespace.SomeClass'));
+        $this->assertEquals(new Clazz('SomeSuperClass'), $classesDependingOnSomeClass->toArray()[0]);
+    }
+
+    public function testDetectsImplementedInterfaces()
+    {
+        $node = new ClassNode('SomeClass');
+        $node->namespacedName = new \stdClass();
+        $node->namespacedName->parts = ['SomeNamespace', 'SomeClass'];
+        $interfaceOneNode = new InterfaceNode('InterfaceOne');
+        $interfaceOneNode->parts = ['Namespace', 'InterfaceOne'];
+        $interfaceTwoNode = new InterfaceNode('InterfaceTwo');
+        $interfaceTwoNode->parts = ['Namespace', 'InterfaceTwo'];
+        $node->implements = [$interfaceOneNode, $interfaceTwoNode];
+
+        $this->dependencyInspectionVisitor->enterNode($node);
+
+        $this->dependencyInspectionVisitor->afterTraverse([]);
+        $classesDependingOnSomeClass = $this->dependencyInspectionVisitor
+            ->dependencies()
+            ->findClassesDependingOn(new Clazz('SomeNamespace.SomeClass'));
+        $this->assertEquals(new Clazz('Namespace.InterfaceOne'), $classesDependingOnSomeClass->toArray()[0]);
+        $this->assertEquals(new Clazz('Namespace.InterfaceTwo'), $classesDependingOnSomeClass->toArray()[1]);
+    }
+
+    public function testDetectsDependenciesFromMethodArguments()
+    {
+        self::fail();
     }
 }
