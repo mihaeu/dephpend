5dc1eb1eeb9997af266db834a7f2e2248f88bb25
Michael Haeuslmann <haeuslmann@gmail.com>
2016-08-30 11:57:37 +0200

    Extract methods and add depth parameter

--- src/DependencyStructureMatrixBuilder.php
+++ src/DependencyStructureMatrixBuilder.php
@@ -11,27 +11,47 @@ class DependencyStructureMatrixBuilder
      *
      * @return array
      */
-    public function buildMatrix(DependencyPairCollection $dependencyPairCollection) : array
+    public function buildMatrix(DependencyPairCollection $dependencyPairCollection, int $depth = 0) : array
     {
-        $dependencies = $dependencyPairCollection
-            ->allDependencies()
-            ->reduce(new DependencyCollection(), function (DependencyCollection $dependencyCollection, Dependency $dependency) {
-                return $dependencyCollection->add($dependency->reduceToDepth(2));
+        $dependencies = $this->allDependenciesReducedByDepth($dependencyPairCollection, $depth);
+        $emptyDsm = $this->createEmptyDsm($dependencies);
+
+        return $dependencyPairCollection->reduce($emptyDsm, function (array $dsm, DependencyPair $dependency) use ($depth) {
+            $from = $dependency->from()->reduceToDepth($depth)->toString();
+            $to = $dependency->to()->reduceToDepth($depth)->toString();
+            $dsm[$from][$to] += 1;
+
+            return $dsm;
+        });
+    }
+
+    /**
+     * @param DependencyPairCollection $dependencyPairCollection
+     *
+     * @return DependencyCollection
+     */
+    private function allDependenciesReducedByDepth(DependencyPairCollection $dependencyPairCollection, int $depth)
+    {
+        return $dependencyPairCollection->allDependencies()->reduce(new DependencyCollection(),
+            function (DependencyCollection $dependencyCollection, Dependency $dependency) use ($depth) {
+                return $dependencyCollection->add($dependency->reduceToDepth($depth));
             }
         );
-        $emptyDsm = $dependencies->reduce([], function (array $combined, Dependency $dependency) use ($dependencies) {
+    }
+
+    /**
+     * @param $dependencies
+     *
+     * @return array
+     */
+    private function createEmptyDsm($dependencies)
+    {
+        return $dependencies->reduce([], function (array $combined, Dependency $dependency) use ($dependencies) {
             $combined[$dependency->toString()] = array_combine(
                 array_values($dependencies->toArray()),     // keys:    dependency name
                 array_pad([], $dependencies->count(), 0)    // values:  [0, 0, 0, ... , 0]
             );
-
             return $combined;
-        });
-
-        return $dependencyPairCollection->reduce($emptyDsm, function (array $dsm, DependencyPair $dependency) {
-            $dsm[$dependency->from()->reduceToDepth(2)->toString()][$dependency->to()->reduceToDepth(2)->toString()] += 1;
-
-            return $dsm;
         });
     }
 }
