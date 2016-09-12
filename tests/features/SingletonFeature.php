f487db1ef74da637dacf774d68fcc3e00dd41565
Michael Haeuslmann <haeuslmann@gmail.com>
2016-09-12 10:31:27 +0200

    Make example more explicit

--- tests/features/SingletonFeature.php
+++ tests/features/SingletonFeature.php
@@ -1,10 +1,28 @@
-# A --> B
+# A --> Singleton
 <?php
 
 class A
 {
     public function test()
     {
-        B::test();
+        Singleton::create();
+    }
+}
+
+class Singleton
+{
+    private static $instance;
+
+    private function __construct()
+    {
+        // ...
+    }
+
+    public static function create()
+    {
+        if (self::$instance === null) {
+            self::$instance = new Singleton();
+        }
+        return self::$instance;
     }
 }
