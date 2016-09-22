eab65b9145b2aede2bb1f1e7d222b70327cff13b
Michael Haeuslmann <haeuslmann@gmail.com>
2016-09-22 15:46:13 +0200

    Quit gracefully after detecting a syntax error

--- src/Analyser/Parser.php
+++ src/Analyser/Parser.php
@@ -6,6 +6,7 @@ namespace Mihaeu\PhpDependencies\Analyser;
 
 use Mihaeu\PhpDependencies\OS\PhpFile;
 use Mihaeu\PhpDependencies\OS\PhpFileSet;
+use PhpParser\Error;
 use PhpParser\Parser as BaseParser;
 
 class Parser
@@ -29,7 +30,14 @@ class Parser
     public function parse(PhpFileSet $files) : Ast
     {
         return $files->reduce(new Ast(), function (Ast $ast, PhpFile $file) {
-            return $ast->add($file, $this->parser->parse($file->code()));
+            try {
+                $nodes = $this->parser->parse($file->code());
+            } catch (Error $e) {
+                echo 'Syntax error during inspection in file "'.$file.'"'.PHP_EOL
+                    .PHP_EOL.$e->getMessage().')'.PHP_EOL;
+                exit;
+            }
+            return $ast->add($file, $nodes);
         });
     }
 }
