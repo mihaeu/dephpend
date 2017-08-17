67a92f11c7aedfd710868e30285ef5c90653917c
Alexander Ustimenko <ustimenko.alexander@gmail.com>
2017-08-17 16:17:40 +0600

    Update regexp in dsm test

--- tests/feature/DsmTest.php
+++ tests/feature/DsmTest.php
@@ -9,7 +9,7 @@ class DsmTest extends BaseTest
     public function testCreatesSimpleDsmInHtml()
     {
         $this->assertRegExp(
-            '/PhpDependencies.+X.+\d.+1.+1\d.+29/s',
+            '/PhpDependencies.+X.+\d.+2.+1\d.+29/s',
             shell_exec(self::DEPHPEND.' dsm '.self::SRC.' --no-classes -d 2 --format=html')
         );
     }
